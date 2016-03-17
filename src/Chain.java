1ec27f09018e6474fdeb5590c5670648eed4d4d7
Aleksandr <sanekzhitnik@gmail.com>
2016-03-17 12:35:06 +0300

    Change class's name

---
+++ src/Chain.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Александр on 13.03.2016.
+ */
+public class Chain {
+    public static void main(String[] args) {
+        SMO m1 = new MultipleHandlerSMO(1.0/3.0,1.0/5.0,2);
+        SMO m2 = new OneHandlerSMO(0.8*1.0/3.0,1.0/2.5);
+
+        System.out.println(m1.coutMv()+m2.coutMv());
+
+        SMO m3 = new MultipleHandlerSMO(0.8*1.0/3.0,1.0/5.0,2);
+        SMO m4 = new OneHandlerSMO(0.8*1.0/3.0,1.0/2.5);
+
+        System.out.println(m3.coutMv()+m4.coutMv());
+
+        SMO m5 = new MultipleHandlerSMO(1.0/3.0,1.0/5.0,3);
+        SMO m6 = new OneHandlerSMO(0.8*1.0/3.0,1.0/2.5);
+
+        System.out.println(m5.coutMv()+m6.coutMv());
+
+    }
+    public static SMO createOneHandlerSMO( double lambda,double mu){
+        SMO smo = new OneHandlerSMO(lambda,mu);
+        return smo;
+    }
+
+    public static SMO createMultipleHandlerSMO(int n, double lambda,double mu){
+        SMO smo = new MultipleHandlerSMO(lambda,mu,n);
+        return smo;
+    }
+
+    public static void viewAllInformation()
+    {};
+}
---
+++ src/MultipleHandlerSMO.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Александр on 13.03.2016.
+ */
+
+public class MultipleHandlerSMO extends SMO{
+    protected int m;
+    MultipleHandlerSMO(double l, double m, int n) {
+        super(l, m, 2);
+        this.m = n;
+    }
+
+    @Override
+    double coutP0() {
+        double sum=0;
+        for (int i = 0; i <=m ; i++) {
+            sum+=Math.pow(ro,i)/fact(i);
+        }
+        sum+=Math.pow(ro,m+1)/(fact(m)*(m-ro));
+
+        return 1/sum;
+    }
+
+    @Override
+    double coutMv() {
+
+        double mw = Math.pow(ro,m+1)/(fact(m)*m*Math.pow((1-ro/m),2))*coutP0()/lamda;
+        double mx = 1/mu;
+        return mw+mx;
+    }
+}
