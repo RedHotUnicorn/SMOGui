065235298e36fbf1f967506d6fe0e46767a7faa1
RedHotUnicorn <sanekzhitnik@gmail.com>
2016-03-24 09:39:52 +0300

    save before remove

--- src/Chain.java
+++ src/Chain.java
@@ -3,10 +3,11 @@
  */
 public class Chain {
     public static void main(String[] args) {
-        SMO m1 = new MultipleHandlerSMO(1.0/3.0,1.0/5.0,2);
-        SMO m2 = new OneHandlerSMO(0.8*1.0/3.0,1.0/2.5);
+        SMO m1 = new MultipleHandlerSMO(1.0,2.0,2);
+       // SMO m2 = new OneHandlerSMO(0.8*1.0/3.0,1.0/2.5);
+        System.out.println(m1.coutP0());
 
-        System.out.println(m1.coutMv()+m2.coutMv());
+       /* System.out.println(m1.coutMv()+m2.coutMv());
 
         SMO m3 = new MultipleHandlerSMO(0.8*1.0/3.0,1.0/5.0,2);
         SMO m4 = new OneHandlerSMO(0.8*1.0/3.0,1.0/2.5);
@@ -16,7 +17,7 @@ public class Chain {
         SMO m5 = new MultipleHandlerSMO(1.0/3.0,1.0/5.0,3);
         SMO m6 = new OneHandlerSMO(0.8*1.0/3.0,1.0/2.5);
 
-        System.out.println(m5.coutMv()+m6.coutMv());
+        System.out.println(m5.coutMv()+m6.coutMv());*/
 
     }
     public static SMO createOneHandlerSMO( double lambda,double mu){
