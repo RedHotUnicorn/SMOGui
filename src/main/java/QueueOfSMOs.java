e3d04f63b4a0ae38145de6462d0c9eb063c09403
Aleksandr <sanekzhitnik@gmail.com>
2016-06-19 03:34:15 +0300

    Add delete vethod to queue, and Method to parse comma in double

--- src/main/java/QueueOfSMOs.java
+++ src/main/java/QueueOfSMOs.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.ParseException;
 import java.util.ArrayList;
 
 /**
@@ -7,31 +11,43 @@ public class QueueOfSMOs {
 
     ArrayList<SMO> queue = new ArrayList<SMO>();
 
-    public void addOneHandlerSMO(double lambda, double mu){
-        SMO smo = new OneHandlerSMO(lambda,mu);
+    public void addOneHandlerSMO(double lambda, double mu) {
+        SMO smo = new OneHandlerSMO(lambda, mu);
         queue.add(smo);
     }
 
-    public void addMultipleHandlerSMO(int chanelCount, double lambda, double mu){
-        SMO smo = new MultipleHandlerSMO(lambda,mu,chanelCount);
+    public void addMultipleHandlerSMO(int chanelCount, double lambda, double mu) {
+        SMO smo = new MultipleHandlerSMO(lambda, mu, chanelCount);
         queue.add(smo);
     }
 
-    public void addLimitedQueueMultipleHandlerSMO(int chanelCount, double lambda, double mu,int queueLimit){
-        SMO smo = new LimitedQueueMultipleHandlerSMO(lambda,mu,chanelCount,queueLimit);
+    public void addLimitedQueueMultipleHandlerSMO(int chanelCount, double lambda, double mu, int queueLimit) {
+        SMO smo = new LimitedQueueMultipleHandlerSMO(lambda, mu, chanelCount, queueLimit);
         queue.add(smo);
     }
 
-    public void calculationIndicators(){
-        double sum=0;
-        for (int i = 0; i <queue.size() ; i++) {
-            sum+=queue.get(i).getMv();
+    public void deleteSMO(int index){
+        try {
+            queue.remove(index);
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("Такого элемента не существует");
         }
-        System.out.println(sum);
     }
 
-    public static void main(String[] args) {
-        SMO m1 = new MultipleHandlerSMO(2.0,1.1,2);
+    public double calculationReactionTimeOffAllQueue() {
+        double sum = 0;
+        for (int i = 0; i < queue.size(); i++) {
+            sum += calculationReactionTimeOffElement(i);
+        }
+        return sum;
+    }
+
+    public double calculationReactionTimeOffElement(int index) {
+        return queue.get(index).getMv();
+    }
+
+    public static void main(String[] args) throws ParseException {
+        /*SMO m1 = new MultipleHandlerSMO(2.0,1.1,2);
         SMO m2 = new OneHandlerSMO(0.8*1.0/3.0,1.0/2.5);
         System.out.println(m1.getP0());
 
@@ -45,8 +61,29 @@ public class QueueOfSMOs {
         SMO m5 = new MultipleHandlerSMO(1.0/3.0,1.0/5.0,3);
         SMO m6 = new OneHandlerSMO(0.8*1.0/3.0,1.0/2.5);
 
-        System.out.println(m5.getMv()+m6.getMv());
-
+        System.out.println(m5.getMv()+m6.getMv());*/
+        QueueOfSMOs q = new QueueOfSMOs();
+        q.addOneHandlerSMO(0.8*1.0/3.0,1.0/2.5);
+        q.addMultipleHandlerSMO(2,2.0,1.1);
+        q.addMultipleHandlerSMO(2,0.8*1.0/3.0,1.0/5.0);
+        q.addOneHandlerSMO(0.8*1.0/3.0,1.0/2.5);
+        q.addMultipleHandlerSMO(3,1.0/3.0,1.0/5.0);
+        q.addOneHandlerSMO(0.8*1.0/3.0,1.0/2.5);
+        System.out.println(q.calculationReactionTimeOffAllQueue());
+        q.deleteSMO(68);
+        double d1=Double.parseDouble("2.68");
+        double d2;
+        try {
+             d2 = Double.parseDouble("2,69");  //метод который парсит запятую вместо точки как разделитьель
+        }catch(NumberFormatException e) {
+            DecimalFormat df = new DecimalFormat();
+            DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+            symbols.setDecimalSeparator(',');
+            symbols.setGroupingSeparator(' ');
+            df.setDecimalFormatSymbols(symbols);
+            d2 = df.parse("2,69").doubleValue();
+        }
+        System.out.println(d1+" "+d2);
     }
 
 }
