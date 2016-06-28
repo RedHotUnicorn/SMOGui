d824c9e429e8c115c1ffb48abc741879ade9d6d8
Aleksandr <sanekzhitnik@gmail.com>
2016-06-28 13:45:59 +0300

    Lisview Settings

--- src/main/java/SMOGUI.java
+++ src/main/java/SMOGUI.java
@@ -1,48 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 /**
  * Created by aleksandr on 18.03.16.
  */
 public class SMOGUI {
     public SMOGUI() {
-//        button1.addActionListener(
-//                new ActionListener() {
-
-//            public void actionPerformed(ActionEvent e) {
-//             textField1.setText (
-//                     (new Double(
-//                             (new QueueOfSMOs())
-//                                     .addOneHandlerSMO(Double.parseDouble(textField2.getText()),Double.parseDouble(textField3.getText()))
-//                                     .getMv())).toString()
-//             );
-//            }
-//        });
-//        button2.addActionListener(new ActionListener() {
-//
-//            public void actionPerformed(ActionEvent e) {
-//                answerText2.setText
-//                        (
-//                        (new Double(
-//                                (new QueueOfSMOs())
-//                                        .addMultipleHandlerSMO(
-//                                                (Integer)spinner1.getValue()
-//                                                ,Double.parseDouble(lambdaText2.getText())
-//                                                ,Double.parseDouble(muText2.getText())
-//                                                                )
-//                                        .getMv()
-//                                    ).toString()
-//                        )
-//                        );
-//            }
-//        });
-
         button2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 
                 JOptionPane.showMessageDialog(MainJPanel, "This is the message", "This is the default text",JOptionPane.ERROR_MESSAGE);
+            }
+        });
+        button1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                list1.setModel(listModel);
+                listModel.addElement(textField2.getText()+" "+textField3.getText());
             }
         });
     }
@@ -53,7 +27,10 @@ public class SMOGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+
     }
+
 
     private JPanel MainJPanel;
     private JButton button1;
@@ -67,7 +44,8 @@ public class SMOGUI {
     private JTextField lambdaText2;
     private JTextField muText2;
     private JTabbedPane tabbedPane2;
-    private JScrollPane Последовательность;
+    DefaultListModel<String> listModel = new DefaultListModel<>();
+    private JList<String> list1;
 
 
     private void createUIComponents() {
